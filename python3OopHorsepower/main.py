091cd601b565d8786e1a8c94db6db988892c210a
Eric Phung <esphung@iupui.edu>
2015-01-31 19:49:33 -0500

    Almost racing

--- python3OopHorsepower/main.py
+++ python3OopHorsepower/main.py
@@ -6,33 +6,69 @@ PURPOSE: 				A horse-racing game in Python3
 '''
 
 from random import randint
+import random
 #print (randint(0,9))
+
 
 class Horse(object):
 	"""Class for creating a Horse"""
 	#print("I am a CLASS horse")
-	def __init__(self, arg, lane):
+	def __init__(self, arg, lane, position):
 		super(Horse, self).__init__()
 		''' generates each horse and lane '''
 		self.arg = arg
 		self.lane = lane
-		''' lane position must change HERE!!! '''
-		lane[0] = self.arg
+		self.position = position
+
+		''' starting positions
+		i = 0
+		while i < 1:
+			lane[0] = self.arg
+			i = i + 1
+		else:
+			lane[0] = lane[0]
+			'''
+
+		''' lane position changes HERE!!! '''
+		
+		lane[position] = self.arg
+		Lane(arg, position, lane)
 		print(lane)
+		
 
 
-
-
-
+class Lane(object):
+	"""docstring for Lane"""
+	def __init__(self, arg, position, lane):
+		super(Lane, self).__init__()
+		self.arg = arg
+		''' here is where the position is defined '''
+		percentage_chance = .5
+		if random.random() < percentage_chance:
+			position = position + 1
+			#print("Yay!!!")
+			lane[position] = self.arg
+			#return
+		
+		
+		
 
 ''' Dev input '''
 #arg = "READY,SET,GO!"
-lane = [0,1,2,3,4,5,6,7,8,9,10]
+position = 0
+count = 0
 
+while count < 10:
+	lane = [0,1,2,3,4,5,6,7,8,9,10]
 
-Horse("Stallion", lane)
-Horse("Mustang",lane)
-Horse("Donkey",lane)
+	Horse("Stallion", lane, position)
+	Horse("Mustang",lane, position)
+	Horse("Donkey",lane, position)
+
+	print("=============================")
+	count = count + 1
+	pass
+
 
 
 
