bb9195227bc294a721769adfd3785f55c25422f3
Eric Phung <esphung@iupui.edu>
2015-01-31 19:04:42 -0500

    W/e

--- python3OopHorsepower/main.py
+++ python3OopHorsepower/main.py
@@ -8,29 +8,6 @@ PURPOSE: 				A horse-racing game in Python3
 from random import randint
 #print (randint(0,9))
 
-class RaceLane(object):
-	"""docstring for RaceLane"""
-	def __init__(self, arg):
-		super(RaceLane, self).__init__()
-		self.arg = arg
-		self.horse = [Horse.stallionHorse(arg), Horse.mustangHorse(arg), Horse.donkeyHorse(arg)]
-		lane = [0,1,2,3,4,5,6,7,8,9,10]
-		j = 0
-		minRand = 0
-		while j < 1:
-			randPosition = (randint(minRand,j))
-			lane[randPosition] = self.horse[0]
-			j = j + 1
-			print(lane)
-		
-		
-		 
-		#i = 0
-		#while i < 3:
-			#print("\t{}".format(self.horse[i]))
-			#i = i + 1
-
-
 
 
 class Horse(object):
@@ -48,26 +25,41 @@ class Horse(object):
 		''' generates a stallion horse '''
 		self.arg = arg
 		self.horseType = "Stallion"
-		return self.horseType
+		lane = [0,1,2,3,4,5,6,7,8,9,10]
+		j = 0
+		minRand = 0
+		randPosition = (randint(minRand,j))
+		lane[randPosition] = self.horseType
+		j = j + 1
+		print(lane)
 
 	@classmethod
 	def mustangHorse(self, arg):
 		''' generates a mustang horse '''
 		self.arg = arg
 		self.horseType = "Mustang"
-		return self.horseType
-		
+		lane = [0,1,2,3,4,5,6,7,8,9,10]
+		j = 0
+		minRand = 0
+		randPosition = (randint(minRand,j))
+		lane[randPosition] = self.horseType
+		j = j + 1
+		print(lane)
 
 	@classmethod
 	def donkeyHorse(self, arg):
 		''' generates a donkey horse '''
 		self.arg = arg
 		self.horseType = "Donkey"
-		return self.horseType
+		lane = [0,1,2,3,4,5,6,7,8,9,10]
+		j = 0
+		minRand = 0
+		randPosition = (randint(minRand,j))
+		lane[randPosition] = self.horseType
+		j = j + 1
+		print(lane)
 		
 		
-
-
 
 
 
@@ -77,17 +69,13 @@ class Horse(object):
 
 
 ''' Dev input '''
-#horseType = "Stallion"
-arg = "READY,SET,GO!"
-#Horse(arg, horseType)
 
-position = 0
+arg = "READY,SET,GO!"
 
 Horse.stallionHorse(arg)
 Horse.mustangHorse(arg)
 Horse.donkeyHorse(arg)
 
-RaceLane(arg)
 
 
 
