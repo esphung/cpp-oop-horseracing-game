da9989e2049c86a154d89665d1bba8ac69a694d6
Eric Phung <esphung@iupui.edu>
2015-02-02 19:41:37 -0500

    Initial commit of c++ version of 'Horse Race'

---
+++ horseRace/horseRace.cpp
@@ -0,0 +1,17 @@
+/* horse race c++ */
+/* Eric Scott Phung */
+/* 2014.02.02 (esp) */
+/* text-based horse racing 
+game in c++ using structures*/
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	cout << "Hello World" << endl;
+	return 0;
+}
